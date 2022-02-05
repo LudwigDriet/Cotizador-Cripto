bdc71755b7b17187dcada613d0e53aa855dead0c
Ludwig Driet <83729472+LudwigDriet@users.noreply.github.com>
2022-02-05 19:15:54 -0300

    nuevo

---
+++ src/componentes/Nuevo.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "@emotion/styled";
+import useMoneda from "../hooks/useMoneda";
+
+
+const Boton = styled.input`
+margin-top:20px;
+margin-left:60px;
+font-weight:bold;
+font-size:20px;
+padding:10px;
+background-color: #66a2fe;
+border:none;
+width:100%;
+border-radius:10px;
+color:#FFF;
+transition: background-color .3s ease;
+
+&:hover{
+    background-color: #326AC0;
+    cursor:pointer;
+}
+`
+
+const Nuevo = ()=>{
+
+    const MONEDAS = [
+        {codigo: 'USD', nombre : 'Dolar de Estados Unidos'},
+        {codigo: 'MXD', nombre : 'Peso Mexicano'},
+        {codigo: 'EUR', nombre : 'Euro'},
+        {codigo: 'GBP', nombre : 'Libra Esterlina'}
+    ]
+
+    const [moneda,Seleccionar,setmoneda]= useMoneda('Elige una moneda','',MONEDAS);
+
+    return(
+
+        <form>
+
+            <Seleccionar/>
+
+            <Boton type='submit' value='Calcular'/>
+        </form>
+    )
+}
+
+export default Nuevo;
