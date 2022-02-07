2ba75de4d64da8cba4eaffb72c70044fe042c7af
Ludwig Driet <83729472+LudwigDriet@users.noreply.github.com>
2022-02-07 19:26:46 -0300

    style select

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,31 @@
 import react ,{Fragment ,useState} from "react";
+import style from '@emotion/styled';
+
+const Label = style.label`
+
+font-family:'Bebas Neue', cursive;
+color: #FFF;
+text-transform: uppercase;
+font-weight:bold;
+font-size:2.4rem;
+margin-top:2rem;
+margin-left:60px;
+display:block;
+
+`;
+
+const Select = style.select`
+
+    width:50%;
+    display:block;
+    padding: 0.5rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border:none;
+    font-size : 0.8rem;
+    margin-left:60px;
+    cursor:pointer;
+`
 
 const useMoneda = (lebel,inicial,MONEDAS)=>{
 
@@ -8,15 +35,18 @@ const useMoneda = (lebel,inicial,MONEDAS)=>{
        
         <Fragment >
 
-            <label style={{marginLeft:'60px'}} >{lebel}</label>
-            <select>
+            <Label >{lebel}</Label>
+            <Select 
+                onChange={ e => setstate(e.target.value)}
+                value={state}
+            >
                 <option vaue=''> Seleccionar </option>
                 {
                     MONEDAS.map (m=>(
                         <option key={m.codigo} vaue={m.codigo}>{m.nombre}</option>
                     ))
                 }
-             </select>
+             </Select>
 
         </Fragment>
     )
